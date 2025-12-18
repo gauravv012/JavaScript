45d2034dda11e19a18fc63871ec3dc1f6a13e8a0
Gaurav sahu <gaurav012sahu@gmail.com>
2025-12-18 18:33:33 +0000

    Math&Date&Error

---
+++ 01_basics/05_Math.js
@@ -0,0 +1,17 @@
+// let curr = new Date();
+
+// let date = new Date('june 30 2006 07:40');
+
+// console.log(date.getDay());
+
+
+// _____________________ErrorHandling______________
+
+ try{
+    console.log(x);
+ }
+
+ catch(err){
+    //what you wangt to do with error
+    throw new Error("Phle declare to kro")
+ }
