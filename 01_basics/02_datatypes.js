3bff6656b09d4581c9929151223fcc9d991877b1
Gaurav sahu <gaurav012sahu@gmail.com>
2025-12-18 08:37:10 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict"//treat all js code as newer version 
+let score = 0
+
+let boolean = Boolean(score)
+// console.log(boolean)
+// console.log(typeof(boolean))
+// console.log(typeof(score))
+
+//******************operation*********************//
+console.log(2**4) 
+
+
