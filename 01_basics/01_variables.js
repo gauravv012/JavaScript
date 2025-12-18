601982ac099c232c687b8af076e75306cbb67be4
Gaurav sahu <gaurav012sahu@gmail.com>
2025-12-18 07:50:40 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1223
+var acountPass ="3457"
+let acountEmail= "gaurav@gmail.com"
+acountCity = "kanpur"
+
+acountEmail = "ved@gmail.com"
+console.log(acountEmail)
+
+acountPass = "6666"
+console.log(acountPass)
+
+console.table([accountId,acountEmail,acountPass,acountCity])
