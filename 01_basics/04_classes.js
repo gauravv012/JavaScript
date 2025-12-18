d802b0bc0155220e0194600f44b6513ff55b572d
Gaurav sahu <gaurav012sahu@gmail.com>
2025-12-18 17:51:18 +0000

    classes&paramaters

---
+++ 01_basics/04_classes.js
@@ -0,0 +1,47 @@
+// class Human{
+//     age = 15;
+//     #wt = 60;//private
+//     Ht = 175;
+
+//     constructor(newAge , newHeight, newWeight){
+//         this.age = newAge;
+//         this.Ht = newHeight;
+//         this.#wt = newWeight;
+
+//     }
+
+//     walking(){
+//         console.log("I am walking" , this.#wt)
+//     }
+
+//     running(){
+//         console.log("I am running")
+//     }
+
+//     get fetchWeight(){
+//         return this.#wt;
+//     }
+
+//     set modifyWeight(val){
+//         this.#wt = val;
+
+//     }
+// }
+
+// let obj = new Human(40,500,101);
+// console.log(obj.fetchWeight);
+// obj.walking();
+
+//*****************function********************* 
+// function sayName(fName = "Gaurav", lName = "Sahu"){
+//     console.log("My name is : ",fName," ",lName);
+// }
+// sayName("Ved", "rajput")
+  
+
+// function solve(value=["love","babbar"]){
+//     console.log("Hello jii", value)
+// }
+// solve(null);
+// solve(undefined);
+
