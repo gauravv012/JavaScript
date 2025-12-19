9c4e745ac57820216c147007494e25951fca9e0b
Gaurav sahu <gaurav012sahu@gmail.com>
2025-12-19 17:55:56 +0000

    performance

---
+++ 01_basics/07_performance.js
@@ -0,0 +1,21 @@
+//code 1
+const t1 = performance.now()
+  for(let i=1;i<=10;i++){
+    let para = document.createElement('p');
+    para.textContent = "This is my car" + i;
+    document.body.appendChild(para);
+  }
+  const t2 = performance.now();
+  console.log("Total time by code1 : " +(t2-t1));
+
+  //code2
+  const t3 = performance.now();
+  let mydiv = document.createElement('div');
+  for(let i=1;i<=10;i++){
+    let para = document.createElement('p');
+    para.textContent="This is my bike " + i;
+    mydiv.appendChild(para);
+  }
+  document.body.appendChild(mydiv)
+  const t4 = performance.now();
+   console.log("Total time by code1 : " +(t4-t3));
